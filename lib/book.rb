3e7a514c861ad6789ea8455f05fb39e1731b5749
Kyle McLean <kyledmclean@gmail.com>
2020-05-09 19:15:47 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,12 @@
 class Book
   
-  attr_accessor :title, :author, :page_count, :genre
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
   
   def initialize(title)
     @title = title
   end
-  
-  def title
-    @title
-  end
+
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
